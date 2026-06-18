068e9feab699bfb16813331b8f39415d68e528d4
Yasin <yasinpeker43@gmail.com>
2026-06-18 09:31:27 +0300

    redirect_views.py is created for redirection to original url from short url(main logic)

---
+++ shortener/views/redirect._views.py
@@ -0,0 +1,4 @@
+from shortener.selectors.selectors import get_active_url_by_code
+from django.shortcuts import redirect
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
