cdbdc5c11cd11080de291496be9ad90818ffa528
Yasin <yasinpeker43@gmail.com>
2026-06-18 09:35:12 +0300

    necessary import are done

--- shortener/views/redirect._views.py
+++ shortener/views/redirect._views.py
@@ -2,3 +2,12 @@ from shortener.selectors.selectors import get_active_url_by_code
 from django.shortcuts import redirect
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
+from django.http import Http404
+from django.db.models import F
+
+
+class RedirectView(APIView):
+    permission_classes=[AllowAny]
+    
+    def get(self,request,short_code):
+        
