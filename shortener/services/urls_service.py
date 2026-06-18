19aa20fcf39e52eed4c62f04156608d4d04664e1
Yasin <yasinpeker43@gmail.com>
2026-06-18 19:57:49 +0300

    added kwargs to the encode_id function

--- shortener/services/urls_service.py
+++ shortener/services/urls_service.py
@@ -4,7 +4,8 @@ from rest_framework.exceptions import ValidationError
 from shortener.utils.sqids_helper import encode_id
 
 
-def create_short_url(*,user,original_url:str,custom_alias:str =None) -> UrlItem:
+def create_short_url(*,user,original_url:str,custom_alias:str = None) -> UrlItem:
+    
     with transaction.atomic():
         if custom_alias:
             if UrlItem.objects.filter(short_url=custom_alias).exists():
@@ -20,12 +21,15 @@ def create_short_url(*,user,original_url:str,custom_alias:str =None) -> UrlItem:
             original_url=original_url,
             user = user
         )
-        url_item.short_url=encode_id(url_item.id)
+        url_item.short_url=encode_id(db_id=url_item.id)
+        
         url_item.save(update_fields=['short_url'])
+        
         
         return url_item
         
 
 def delete_short_url(*,url_item:UrlItem)->None:
-    url_item.delete()
+    
+   url_item.delete()
     
