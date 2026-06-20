e0ca92f23225373210ed8adb754efbf5e752247a
Yasin <yasinpeker43@gmail.com>
2026-06-20 21:42:58 +0300

    refactor redirect endpoint with celery & redis connection

--- shortener/views/redirect_views.py
+++ shortener/views/redirect_views.py
@@ -1,22 +1,29 @@
+from rest_framework.permissions import AllowAny
+from django.core.cache import cache
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from shortener.selectors.selectors import get_active_url_by_code
 from django.shortcuts import redirect
-from rest_framework.permissions import AllowAny
-from rest_framework.views import APIView
-from django.http import Http404
-from django.db.models import F
 
 
 class RedirectAPIView(APIView):
     permission_classes=[AllowAny]
+    authentication_classes=[]
     
     def get(self,request,short_code):
         
         url_item = get_active_url_by_code(short_code=short_code)
         
-        if not url_item:
-            raise Http404("short url is not found or expires.")
+        if url_item:
+            
+            cache.incr(f"clicks_{short_code}")
+            return redirect(url_item.original_url)
         
-        url_item.click_count=F('click_count') + 1
-        url_item.save(update_fields=['click_count'])
+        else:
+            return Response({"error":"Url is not found or expired."}, status=status.HTTP_404_NOT_FOUND)
         
-        return redirect(url_item.original_url)
+        
+        
+    
+       
