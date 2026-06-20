7e5c29b6bfc026dad48286f737ccd565241c67b2
Yasin <yasinpeker43@gmail.com>
2026-06-20 21:29:40 +0300

    creating tasks.py

--- shortener/tasks.py
+++ shortener/tasks.py
@@ -9,3 +9,27 @@ logger = logging.getLogger(__name__)
 @shared_task
 def sync_click_counts():
     
+    keys = cache.keys('click_*')
+    
+    if not keys:
+        logger.info("There's no count info to synchronize")
+        return 'No transcation needed.'
+
+    updated_count = 0
+    
+    for key in keys:
+        short_code = key.split('click_')[-1]
+        clicks = cache.get(key)
+        
+        if clicks and int(clicks) > 0:
+            try:
+                updated_rows = UrlItem.objects.filter(short_url = short_code).update(
+                    click_count =F('click_count') + int(clicks)
+                )
+                if updated_rows > 0:
+                    cache.delete(key)
+                    updated_count += 1
+                else:
+                    logger.warning("")
+        
+        
